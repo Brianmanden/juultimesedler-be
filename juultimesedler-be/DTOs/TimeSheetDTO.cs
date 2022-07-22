e183568f559cf771fc73a086499ff60b9e6a6141
Brian Andersen <brianmanden@gmail.com>
2022-07-22 20:49:12 +0200

    ADD TimeSheetDTO

---
+++ juultimesedler-be/DTOs/TimeSheetDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace juultimesedler_be.DTOs
+{
+    public class TimeSheetDTO
+    {
+        public string SelectedProjectAdvanced{ get; set; }
+        public string[] SelectedTasks { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string JobDesc { get; set; }
+    }
+}
