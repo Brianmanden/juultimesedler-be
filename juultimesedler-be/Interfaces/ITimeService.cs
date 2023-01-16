aed7d12d3707a4646dd9acaf1dd06bf7038b2b83
Brian Andersen <brianmanden@gmail.com>
2023-01-16 15:46:15 +0100

    ADD TimeService and interface

---
+++ juultimesedler-be/Interfaces/ITimeService.cs
@@ -0,0 +1,9 @@
+﻿namespace juultimesedler_be.Interfaces
+{
+    public interface ITimeService
+    {
+        public string FormattedCurrentWeek();
+
+        public string FormattedCurrentYearAndWeek(DateTime? date);
+    }
+}
---
+++ juultimesedler-be/Services/TimeService.cs
@@ -0,0 +1,30 @@
+﻿using juultimesedler_be.Interfaces;
+using System.Globalization;
+
+namespace juultimesedler_be.Services
+{
+    public class TimeService : ITimeService
+    {
+        public string FormattedCurrentWeek()
+        {
+            string formattedWeekString =
+                ISOWeek.GetWeekOfYear(DateTime.Now) < 10
+                    ? "0" + ISOWeek.GetWeekOfYear(DateTime.Now).ToString()
+                    : ISOWeek.GetWeekOfYear(DateTime.Now).ToString();
+
+            return formattedWeekString;
+        }
+
+        public string FormattedCurrentYearAndWeek(DateTime? date = null)
+        {
+            date = date ?? DateTime.Now;
+
+            string yearNumber = ISOWeek.GetYear((DateTime)date).ToString();
+            string weekNumber = ISOWeek.GetWeekOfYear((DateTime)date) < 10
+                ? "0" +  ISOWeek.GetWeekOfYear((DateTime)date).ToString()
+                : ISOWeek.GetWeekOfYear((DateTime)date).ToString();
+
+            return $"{yearNumber}_{weekNumber}";
+        }
+    }
+}
