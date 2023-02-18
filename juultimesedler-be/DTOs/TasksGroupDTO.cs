c8a1f1bc019c98442608630e7fb2ee97cef6717f
Brian Andersen <brianmanden@gmail.com>
2023-02-18 16:12:47 +0100

    ADD TasksGroupDTO

---
+++ juultimesedler-be/DTOs/TasksGroupDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace juultimesedler_be.DTOs
+{
+    public class TasksGroupDTO
+    {
+        public string TaskGroupName { get; set; }
+        public List<string> TaskNames { get; set; } = new List<string>();
+    }
+}
