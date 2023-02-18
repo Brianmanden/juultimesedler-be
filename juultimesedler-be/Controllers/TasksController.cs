dca71a1ee9410936a4baa6b2ca365778fc6973a5
Brian Andersen <brianmanden@gmail.com>
2023-02-18 16:15:05 +0100

    ADD TasksController

---
+++ juultimesedler-be/Controllers/TasksController.cs
@@ -0,0 +1,48 @@
+﻿using juultimesedler_be.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Common;
+
+namespace juultimesedler_be.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly UmbracoHelper _umbracoHelper;
+        private IContentService _contentService;
+
+        public TasksController(UmbracoHelper umbracoHelper, IContentService contentService)
+        {
+            _umbracoHelper = umbracoHelper;
+            _contentService = contentService;
+        }
+
+        [HttpGet("api/tasks/")]
+        public List<TasksGroupDTO> GetAllTasks()
+        {
+            List<TasksGroupDTO> groupedTasks = new List<TasksGroupDTO>();
+
+            IPublishedContent rootNode = _umbracoHelper.ContentAtRoot().DescendantsOrSelfOfType("tasks").FirstOrDefault();
+            IList<IPublishedContent> tasksGroup = rootNode.Children().DescendantsOrSelfOfType("tasksGroup").ToList();
+
+            if (tasksGroup.Count < 1) {
+                return groupedTasks;
+            }
+
+            foreach (var taskGroup in tasksGroup) {
+                var currentTaskGroup = new TasksGroupDTO();
+
+                currentTaskGroup.TaskGroupName = taskGroup.Name!;
+
+                foreach (string taskName in taskGroup.GetProperty("TaskList").GetValue().ToString().Split("\n"))
+                {
+                    currentTaskGroup.TaskNames.Add(taskName);
+                }
+
+                groupedTasks.Add(currentTaskGroup);
+            }
+
+            return groupedTasks;
+        }
+    }
+}
