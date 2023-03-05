c12cd1055e8bfc0012908c538292150ce1514788
Brianmanden <brianmanden@gmail.com>
2023-03-05 18:31:32 +0100

    REFACTOR jobDesc => taskComments

--- juultimesedler-be/DTOs/TimeSheetDTO.cs
+++ juultimesedler-be/DTOs/TimeSheetDTO.cs
@@ -3,9 +3,9 @@
     public class TimeSheetDTO
     {
         public int SelectedProjectId { get; set; }
+        public string EndTime { get; set; }
         public string[] SelectedTasks { get; set; }
         public string StartTime { get; set; }
-        public string EndTime { get; set; }
-        public string JobDesc { get; set; }
+        public string TaskComments { get; set; }
     }
 }
