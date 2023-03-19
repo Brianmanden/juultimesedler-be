abf2f49728894f0c65e60029bbb9207d4404ed8f
Brian Andersen <brianmanden@gmail.com>
2023-03-19 12:56:39 +0100

    REFACTOR and ADD models and DTOs

---
+++ juultimesedler-be/DTOs/PutTimeSheetDTO.cs
@@ -0,0 +1,12 @@
+﻿using juultimesedler_be.Models;
+using System.Collections.Generic;
+
+namespace juultimesedler_be.DTOs
+{
+    public class PutTimeSheetDTO
+    {
+        public int WorkerId { get; set; }
+        public int WeekNumber { get; set; }
+        public List<WorkDay>? Workdays { get; set; }
+    }
+}
--- juultimesedler-be/Models/WorkDay.cs
+++ juultimesedler-be/Models/WorkDay.cs
@@ -1,11 +1,12 @@
-﻿namespace juultimesedler_be.DTOs
+﻿namespace juultimesedler_be.Models
 {
-    public class TimeSheetDTO
+    public class WorkDay
     {
+        public int WeekDay { get; set; }
         public int SelectedProjectId { get; set; }
-        public string EndTime { get; set; }
         public string[] SelectedTasks { get; set; }
         public string StartTime { get; set; }
+        public string EndTime { get; set; }
         public string TaskComments { get; set; }
     }
 }
