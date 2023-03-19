0f0230ef75eac607909414abb1ffa8693df754c9
Brian Andersen <brianmanden@gmail.com>
2023-03-19 15:57:33 +0100

    ADD GetCurrentTimesheetweek method to TimeService

---
+++ juultimesedler-be/DTOs/GetTimesheetWeekDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace juultimesedler_be.DTOs
+{
+    public class GetTimesheetWeekDTO
+    {
+        public int WeekNumber { get; set; }
+        public string[] WeekDays { get; set; }
+        public int[] WeekDates { get; set; }
+    }
+}
--- juultimesedler-be/Services/TimeService.cs
+++ juultimesedler-be/Services/TimeService.cs
@@ -1,10 +1,35 @@
-﻿using juultimesedler_be.Interfaces;
+﻿using juultimesedler_be.DTOs;
+using juultimesedler_be.Interfaces;
 using System.Globalization;
 
 namespace juultimesedler_be.Services
 {
     public class TimeService : ITimeService
     {
+        private int WeekNumber()
+        {
+            CultureInfo cultureInfo = new CultureInfo("da-DK");
+            Calendar cal = cultureInfo.Calendar;
+            CalendarWeekRule calendarWeekRule = cultureInfo.DateTimeFormat.CalendarWeekRule;
+
+            int weekNumber = cal.GetWeekOfYear(DateTime.Now, calendarWeekRule, DayOfWeek.Monday);
+            return weekNumber;
+        }
+
+        private int[] WeekDates(int weekNumber)
+        {
+            int[] dates = new int[] {
+                ISOWeek.ToDateTime(DateTime.Now.Year, weekNumber, DayOfWeek.Monday).Day,
+                ISOWeek.ToDateTime(DateTime.Now.Year, weekNumber, DayOfWeek.Monday).AddDays(1).Day,
+                ISOWeek.ToDateTime(DateTime.Now.Year, weekNumber, DayOfWeek.Monday).AddDays(2).Day,
+                ISOWeek.ToDateTime(DateTime.Now.Year, weekNumber, DayOfWeek.Monday).AddDays(3).Day,
+                ISOWeek.ToDateTime(DateTime.Now.Year, weekNumber, DayOfWeek.Monday).AddDays(4).Day,
+                ISOWeek.ToDateTime(DateTime.Now.Year, weekNumber, DayOfWeek.Monday).AddDays(5).Day,
+                ISOWeek.ToDateTime(DateTime.Now.Year, weekNumber, DayOfWeek.Monday).AddDays(6).Day,
+            };
+            return dates;
+        }
+
         public string FormattedCurrentWeek()
         {
             string formattedWeekString =
@@ -26,5 +51,19 @@ namespace juultimesedler_be.Services
 
             return $"{yearNumber}_{weekNumber}";
         }
+
+        public GetTimesheetWeekDTO GetCurrentTimesheetWeek()
+        {
+            int weekNumber = WeekNumber();
+            GetTimesheetWeekDTO currentTimesheetWeek = new();
+            currentTimesheetWeek.WeekNumber = weekNumber;
+            currentTimesheetWeek.WeekDays = new string[]{
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+            };
+            currentTimesheetWeek.WeekDates = WeekDates(weekNumber);
+
+            return currentTimesheetWeek;
+        }
+
     }
 }
